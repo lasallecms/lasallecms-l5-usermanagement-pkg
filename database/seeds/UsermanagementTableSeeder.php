1c8bcf02e3479dd2a11ea05bf346c20f4d323c80
Bob Bloom <info@southlasalle.com>
2015-02-26 02:12:06 -0500

    Fix type in seeder class name. #24

--- database/seeds/UsermanagementTableSeeder.php
+++ database/seeds/UsermanagementTableSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
-class UsermanagementSeeder extends Seeder {
+class UsermanagementTableSeeder extends Seeder {
 
 	/**
 	 * Run the database seeds.
